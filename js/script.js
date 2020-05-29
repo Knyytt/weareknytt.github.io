6a54850fbe1068b9a2db0ceb133643df3ec96e0b
Knyytt <37240139+Knyytt@users.noreply.github.com>
2020-05-29 11:59:51 +0200

    Separated .js files
    Moved page specific js to separate file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-//Testimonials carousel
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 8000); // Change image every 8 seconds
-}
-
 //Menu button click
 function menuButton() {
   var hex = document.getElementsByClassName("hidden-hex");
