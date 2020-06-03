6ac330b8df56c49a1e6863a7dc1dc845e293cd47
Knyytt <37240139+Knyytt@users.noreply.github.com>
2020-06-03 11:03:49 +0200

    Upated photography page

--- photography/photography.js
+++ photography/photography.js
@@ -1,11 +1,17 @@
 var slideIndex = 0;
 var slideIndex2 = 0;
 var slideIndex3 = 0;
+var slideIndex4 = 0;
+var slideIndex5 = 0;
+var slideIndex6 = 0;
 carousel();
 carousel2();
 carousel3();
+carousel4();
+carousel5();
+carousel6();
 
-//Photography carousel
+//Photography carousel eye-catching
 function carousel() {
   var i;
   var x = document.getElementsByClassName("slide1");
@@ -63,4 +69,64 @@ function carousel3() {
   if (slideIndex3 > x.length) {slideIndex3 = 1}
   x[slideIndex3-1].style.display = "block";
   setTimeout(carousel3, 9000); // Change image every 8 seconds
+}
+
+//Photography carousel clean
+function carousel4() {
+  var i;
+  var x = document.getElementsByClassName("slide4");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex4++;
+  if (slideIndex4 > x.length) {slideIndex4 = 1}
+  x[slideIndex4-1].style.display = "block";
+  setTimeout(carousel41, 3000); // Change image every 8 seconds
+}
+function carousel41() {
+  var i;
+  var x = document.getElementsByClassName("slide4");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex4++;
+  if (slideIndex4 > x.length) {slideIndex4 = 1}
+  x[slideIndex4-1].style.display = "block";
+  setTimeout(carousel41, 9000); // Change image every 8 seconds
+}
+
+function carousel5() {
+  var i;
+  var x = document.getElementsByClassName("slide5");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex5++;
+  if (slideIndex5 > x.length) {slideIndex5 = 1}
+  x[slideIndex5-1].style.display = "block";
+  setTimeout(carousel51, 6000); // Change image every 8 seconds
+}
+
+function carousel51() {
+  var i;
+  var x = document.getElementsByClassName("slide5");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex5++;
+  if (slideIndex5 > x.length) {slideIndex5 = 1}
+  x[slideIndex5-1].style.display = "block";
+  setTimeout(carousel51, 9000); // Change image every 8 seconds
+}
+
+function carousel6() {
+  var i;
+  var x = document.getElementsByClassName("slide6");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex6++;
+  if (slideIndex6 > x.length) {slideIndex6 = 1}
+  x[slideIndex6-1].style.display = "block";
+  setTimeout(carousel6, 9000); // Change image every 8 seconds
 }
